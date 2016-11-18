1f298bfb5f37ee95947bd85e6b3ce52300c6b79e
Peter <bloodes11insti@gmail.com>
2016-11-18 12:10:32 +0100

    añadir el archivo Holamundo sin documentación

--- Holamundo.java
+++ Holamundo.java
@@ -1,3 +1,8 @@
+/**Clase holamundo 
+para empezar a entendert git
+@author Pedro
+@version 1.4
+*/
 public class Holamundo{
 	public static void main(String args[]){
 		System.out.println("Hola tonto");
